25d4f1575d8a558ffd6ec1f4c5fd3a65394f4f17
Sujeet Kohli <sujeetkohli@yahoo.com>
2025-10-03 18:03:28 -0400

    Delete look_dbt/dbt_packages directory

--- look_dbt/dbt_packages/dbt_unit_testing/integration-tests/models/biquery/model_with_quoted_columns_1_bq.sql
+++
@@ -1,6 +0,0 @@
-select  10 as lower,
-        20 as UPPER,
-        30 as MiXeD,
-        40 as `lower quoted`,
-        50 as `UPPER QUOTED`,
-        60 as `MiXeD Quoted`
--- look_dbt/dbt_packages/dbt_unit_testing/integration-tests/models/model_with_quoted_columns_1.sql
+++
@@ -1,6 +0,0 @@
-select  1 as lower,
-        2 as UPPER,
-        3 as MiXeD,
-        4 as "lower quoted",
-        5 as "UPPER QUOTED",
-        6 as "MiXeD Quoted"
